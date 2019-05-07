fdbfb1bf16ea7aab5bc0bce006debf56ca5db2eb
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2019-05-07 16:01:11 +0700

    Initial Commit

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by bagasstb on 07/05/19.
+//  Copyright © 2019 xProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
