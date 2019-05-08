0861755014b90f4e6324422215bceda4bdb6adba
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2019-05-08 12:37:56 +0700

    - Set up UI - Implement Slider, Play, and Stop

--- iMusic/ViewController.swift
+++ iMusic/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer = AVAudioPlayer()
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var playButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "sample", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+            setSlider(max: audioPlayer.duration)
+            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateAudioProgress), userInfo: nil, repeats: true)
+            audioPlayer.play()
+        } catch {
+            print(error)
+        }
     }
-
-
+    
+    @objc func updateAudioProgress() {
+        if audioPlayer.isPlaying{
+            slider.setValue(Float(audioPlayer.currentTime), animated: true)
+        }
+    }
+    
+    private func setSlider(max: TimeInterval) {
+        slider.maximumValue = Float(max)
+    }
+    
+    @IBAction func playPressed(_ sender: UIButton) {
+        if audioPlayer.isPlaying {
+            audioPlayer.stop()
+            setImageButton(named: "Play")
+        } else {
+            audioPlayer.play()
+            setImageButton(named: "Stop")
+        }
+    }
+    
+    private func setImageButton(named: String) {
+        playButton.setBackgroundImage(UIImage(named: named), for: .normal)
+    }
+    
+    @IBAction func nextPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func prevButton(_ sender: UIButton) {
+        
+    }
+    
 }
 
